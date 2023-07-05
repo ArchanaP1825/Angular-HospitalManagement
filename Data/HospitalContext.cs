3ef2ea9aad5ba94d67e354ec93e0ee3b77aa99f7
ArchanaP1825 <109856553+ArchanaP1825@users.noreply.github.com>
2023-07-05 22:01:33 +0530

    Add files via upload

---
+++ Data/HospitalContext.cs
@@ -0,0 +1,16 @@
+﻿using Hospital.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Data
+{
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<DoctorDetails> DoctorDetails { get; set; }
+        public DbSet<Approval> Approvals { get; set; }
+    }
+}
